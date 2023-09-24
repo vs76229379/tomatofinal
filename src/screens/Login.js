14eeb9a360031c793e31a60b78f25e1a50bb6527
vs76229379 <104732165+vs76229379@users.noreply.github.com>
2023-09-24 15:35:27 +0530

    updated

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   let navigate = useNavigate()
   const handlesubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("https://tomatonew.onrender.com/api/loginuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
