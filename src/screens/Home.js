e2352afc6beb574104188735503520715ed1b76c
vs76229379 <104732165+vs76229379@users.noreply.github.com>
2024-09-22 00:10:26 +0530

    new22

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [foodItems, setFoodItems] = useState([])
   const [search, setSearch] = useState('')
   const loadFoodItems = async () => {
-    let response = await fetch("http://localhost:5000/api/auth/foodData", {
+    let response = await fetch("https://tomatomernappbackend.onrender.com/api/foodData", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
